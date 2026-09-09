55b06c9880e2741d35fb232a8d33b15d86b0d5f2
emmanuellauloko240-jpg <emmanuellauloko240@gmail.com>
2026-09-09 23:37:09 +0100

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,23 +1,26 @@
-    while True:
-        try:
-            num1 = int(input("Enter first number: "))
-            num2 = int(input("Enter second number: "))
-            operation = input("Enter operation:(+-/*) ")
-            if operation == "+":
-                print("Result: ", num1 + num2)
-            elif operation == "-":
-                print("Result: ", num1 - num2)
-            elif operation == "*":
-                print("Result: ", num1 * num2)
-            elif operation == "/":
-                if num2 == 0:
-                    print("Error: Cannot divide by zero")
-                else:
-                    print("Result:", num1 / num2)
+while True:
+    try:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        operation = input("Enter operation:(+,-,/,*) ")
+        
+        if operation == "+":
+            result = num1 + num2
+        elif operation == "-":
+            result = num1 - num2
+        elif operation == "*":
+            result = num1 * num2
+        elif operation == "/":
+            if num2 == 0:
+                    result = "Error: Cannot divide by zero"
             else:
-                print("Invalid operation")
-        except ValueError:
-            print("that's not a valid number. Please enter digits only.")
-        again = input("Do you want to calculate again? (yes/no): ").lower()
-        if again == "no" or again == "n":
-            break
+                result = num1 / num2
+        else:
+            result = "Invalid operation"
+
+    # except ValueError:
+    #     print("that's not a valid number. Please enter digits only.")
+    
+    again = input("Do you want to calculate again? (yes/no): ").lower()
+    if again == "no" or again == "n":
+        break
