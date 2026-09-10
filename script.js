9f01b391408f0bdf17b1530d0459f3aa85ad6856
emmanuellauloko240-jpg <emmanuellauloko240@gmail.com>
2026-09-10 16:13:42 +0100

    cal

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("JavaScript connected!!!");
+
+let number1 = 10;
+let number2 = 5;
+console.log(number1+number2);
+document.getElementById("display");
+let display = document.getElementById("display")
+display.value = 123;
+let seven = document.getElementById("seven")
+seven.addEventListener("click")
