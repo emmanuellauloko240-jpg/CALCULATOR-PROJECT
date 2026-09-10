b11c6b1ad93021fafa8ba646263a144114035e52
emmanuellauloko240-jpg <emmanuellauloko240@gmail.com>
2026-09-10 12:07:27 +0100

    cal

--- calculator.py
+++ calculator.py
@@ -1,26 +1,31 @@
+def calculator(num1, num2, operation):
+                if operation == "+":
+                    result = num1 + num2
+                    # print(result)
+                elif operation == "-":
+                    result = num1 - num2
+                    # print(result)
+                elif operation == "*":
+                    result = num1 * num2
+                    # print(result)
+                elif operation == "/":
+                    if num2 == 0:
+                            result = "Error: Cannot divide by zero"
+                    else:
+                        result = num1 / num2
+                else:
+                    result = "Invalid operation"
+                return result
+
 while True:
-    try:
+
         num1 = int(input("Enter first number: "))
         num2 = int(input("Enter second number: "))
-        operation = input("Enter operation:(+,-,/,*) ")
-        
-        if operation == "+":
-            result = num1 + num2
-        elif operation == "-":
-            result = num1 - num2
-        elif operation == "*":
-            result = num1 * num2
-        elif operation == "/":
-            if num2 == 0:
-                    result = "Error: Cannot divide by zero"
-            else:
-                result = num1 / num2
-        else:
-            result = "Invalid operation"
+        operation = input("Enter operation (-,+,*,/): ")
+        answer = calculator(num1,num2,operation)
+        print(answer)
 
-    # except ValueError:
-    #     print("that's not a valid number. Please enter digits only.")
-    
-    again = input("Do you want to calculate again? (yes/no): ").lower()
-    if again == "no" or again == "n":
-        break
+        # answer = calculator(____, ____, ____)
+        again = input("Do you want to calculate again? (yes/no): ").lower()
+        if again == "no" or again == "n":
+            break
