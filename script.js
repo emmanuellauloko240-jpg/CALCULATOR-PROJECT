af9866fe8a834e9f40b892f3db3195a5a8bd709a
emmanuellauloko240-jpg <emmanuellauloko240@gmail.com>
2026-09-11 14:09:01 +0100

    Implement button click handling for display input
    Added button event listeners for calculator functionality.

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ console.log("JavaScript connected!!!");
 let number1 = 10;
 let number2 = 5;
 console.log(number1+number2);
-document.getElementById("display");
 let display = document.getElementById("display")
-display.value = 123;
 let seven = document.getElementById("seven")
-seven.addEventListener("click")
+let buttons = document.querySelectorAll("button")
+let firstNumber;
+let operation;
+for (let button of buttons){
+    button.addEventListener("click",function(event) {
+        display.value = display.value + event.target.value;
+        if isNaN(event.target.value)
+        if (event.target.value === "="){
+    
+        }else{
+
+        }
+        firstNumber = event.target.value;
+    })
+}
+
