23d720d92a54632c17395a7782d0960d8c9f6358
emmanuellauloko240-jpg <emmanuellauloko240@gmail.com>
2026-09-14 17:57:58 +0100

    script file

--- script.js
+++ script.js
@@ -1,59 +1,97 @@
-console.log("JavaScript connected!!!");
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
 
-let number1 = 10;
-let number2 = 5;
-console.log(number1+number2);
-let display = document.getElementById("display")
-let seven = document.getElementById("seven")
-let buttons = document.querySelectorAll("button")
 let firstNumber;
 let operation;
 let secondNumber;
+let justCalculated = false;
 
-for (let button of buttons){
-    button.addEventListener("click",function(event) {
-        display.value = display.value + event.target.value;
-        if (["+","-","*","/"].includes(event.target.value)){
-            operation = event.target.value
-        }
-        if (!isNaN(event.target.value)){
-            if (operation === undefined){
-                if (firstNumber === undefined) {
-                    firstNumber = Number(event.target.value)
-                }else{
-                    firstNumber = event.target.value + Number(event.target.value)
-                    }
-            
-            }else{
-            secondNumber = Number(event.target.value)
-        }
+function calculate() {
+    switch (operation) {
+        case "+":
+            display.value = firstNumber + secondNumber;
+            break;
+
+        case "-":
+            display.value = firstNumber - secondNumber;
+            break;
+
+        case "*":
+            display.value = firstNumber * secondNumber;
+            break;
+
+        case "/":
+            if (secondNumber === 0) {
+                display.value = "Cannot divide by zero";
+            } else {
+                display.value = firstNumber / secondNumber;
+            }
+            break;
     }
-        if (event.target.value === "="){
-            if (operation === "+"){
-                display.value = firstNumber + secondNumber
-        }
-            if (operation === "-"){
-                display.value = firstNumber - secondNumber
-        }
-            if (operation === "*"){
-                display.value = firstNumber * secondNumber
-        }
-            if (operation === "/"){
-                display.value = firstNumber / secondNumber
-        }
-        
-         
-            
-
-            firstNumber = undefined;
-            operation = undefined;
-            secondNumber = undefined;
-            console.log(firstNumber);
-            console.log(operation);
-            console.log(secondNumber);
-        }else{
-
-        }
-    })
 }
 
+function clearCalculator() {
+    display.value = "";
+    firstNumber = undefined;
+    operation = undefined;
+    secondNumber = undefined;
+    justCalculated = false;
+}
+
+function handleNumber(value) {
+    if (justCalculated) {
+        display.value = value;
+        firstNumber = Number(value);
+        operation = undefined;
+        secondNumber = undefined;
+        justCalculated = false;
+        return;
+    }
+
+    display.value = display.value + value;
+
+    if (operation === undefined) {
+        if (firstNumber === undefined) {
+            firstNumber = Number(value);
+        } else {
+            firstNumber = Number(String(firstNumber) + value);
+        }
+    } else {
+        if (secondNumber === undefined) {
+            secondNumber = Number(value);
+        } else {
+            secondNumber = Number(String(secondNumber) + value);
+        }
+    }
+}
+
+function handleOperator(value) {
+    if (justCalculated) {
+        firstNumber = Number(display.value);
+        secondNumber = undefined;
+        justCalculated = false;
+    }
+
+    operation = value;
+    display.value = display.value + value;
+}
+
+for (let button of buttons) {
+    button.addEventListener("click", function(event) {
+        let value = event.target.value;
+
+        if (["+", "-", "*", "/"].includes(value)) {
+            handleOperator(value);
+
+        } else if (!isNaN(value)) {
+            handleNumber(value);
+
+        } else if (value === "=") {
+            calculate();
+            justCalculated = true;
+
+        } else if (value === "C") {
+            clearCalculator();
+        }
+    });
+}
